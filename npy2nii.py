9dfdf4919368726e9602e4ca53fd55376bd5ff5b
Sando <comsicomet@gmail.com>
2022-06-02 16:18:09 +0900

    Update npy2nii.py

--- npy2nii.py
+++ npy2nii.py
@@ -1,8 +1,10 @@
 import numpy as np
 import nibabel as nib
 
-converted_array = numpy.array(normal_array, dtype=numpy.float32) # You need to replace normal array by yours
-affine = numpy.eye(4)
-nifti_file = nibabel.Nifti1Image(converted_array, affine)
+path_to_save = '../..nii'
 
-nibabel.save(nifti_file, path_to_save) # Here you put the path + the extionsion 'nii' or 'nii.gz'
+converted_array = np.array(normal_array, dtype=np.float32) # You need to replace normal array by yours
+affine = np.eye(4)
+nifti_file = nib.Nifti1Image(converted_array, affine)
+
+nib.save(nifti_file, path_to_save) # Here you put the path + the extionsion 'nii' or 'nii.gz'
